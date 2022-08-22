4e7439b4d33bb9b75c7027594215bbd33bf47b5b
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-22 18:20:00 +0600

    displaySelectedPlayers from array

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 // total selected player array for counting
 const playerTotalArray = [];
 
+//  displaying selected players
+function displaySelectedPlayers(playerTotalArray) {
+  console.log(playerTotalArray);
+  // console.log(playerTotalArray.length);
+  if (playerTotalArray.length > 5) {
+    alert("You can only select 5 players");
+    playerTotalArray.pop();
+    return;
+  }
+  // find out player name from array
+  // const playerName =
+  for (const player of playerTotalArray) {
+    console.log(player.playerName);
+  }
+}
+
 function selectPlayer(element) {
   const playerName = element.parentNode.children[1].innerText;
   //   creating player name Object
@@ -10,8 +26,11 @@ function selectPlayer(element) {
   //   pushing player name object to playerTotalArray
   playerTotalArray.push(playerNameObj);
 
+  displaySelectedPlayers(playerTotalArray);
+
   //   if total selected player is more than 5 then it will show alert
-  if (playerTotalArray.length > 5) {
-    alert("You can only select 5 players");
-  }
+  // if (playerTotalArray.length > 5) {
+  //   alert("You can only select 5 players");
+  //   // return;
+  // }
 }
